5545e3366700e05a9d7214db33df82601ffef8f4
jhyun <arsgsg1@gmail.com>
2023-02-19 21:52:03 +0900

    코드 리팩토링

--- src/main/kotlin/com/numble/team2/controller/MemberController.kt
+++ src/main/kotlin/com/numble/team2/controller/MemberController.kt
@@ -8,26 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/members")
 class MemberController(
     private val memberService: MemberService
 ) {
-    @PostMapping("/members")
+    @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     fun SignUp(@RequestBody dto: SignUpRequest) {
         memberService.createMember(dto)
     }
 
-    @PostMapping("/members/{memberId}/friends")
+    @PostMapping("/{memberId}/friends")
     @ResponseStatus(code = HttpStatus.CREATED)
     fun createMemberFriend(@PathVariable memberId: Long, @RequestBody dto: MemberFriendRequest) {
         memberService.createFriend(memberId, dto)
     }
 
-    @GetMapping("/members/{memberId}/friends")
+    @GetMapping("/{memberId}/friends")
     @ResponseStatus(code = HttpStatus.OK)
     fun getAllMemberFriends(@PathVariable memberId: Long): List<Long> {
         return memberService.getAllFriends(memberId)
--- src/main/kotlin/com/numble/team2/service/MemberService.kt
+++ src/main/kotlin/com/numble/team2/service/MemberService.kt
@@ -15,13 +15,13 @@ class MemberService(
     private val memberRepository: MemberRepository,
 ) {
     @Transactional(readOnly = true)
-    fun checkNotDuplicateEmail(email: String): Boolean {
+    fun isNotDuplicateEmail(email: String): Boolean {
         return !memberRepository.findByEmail(email).isPresent
     }
 
     @Transactional
     fun createMember(dto: SignUpRequest): Long {
-        require(checkNotDuplicateEmail(dto.email)) { "이미 존재하는 이메일입니다." }
+        require(isNotDuplicateEmail(dto.email)) { "이미 존재하는 이메일입니다." }
         val memberEntity = MemberEntity(email = dto.email, password = dto.password)
         return memberRepository.save(memberEntity).id
     }
@@ -31,9 +31,7 @@ class MemberService(
         val memberEntity = memberRepository.findMemberWithAllFriends(memberId)
             .orElseThrow { throw IllegalArgumentException("해당 ID의 유저가 없습니다. [${memberId}]") }
         memberEntity.friends.firstOrNull { entity -> entity.friendId == dto.friendId } ?: run {
-            val memberFriendEntity =
-                MemberFriendEntity(memberId = memberId, friendId = dto.friendId)
-            memberEntity.friends.add(memberFriendEntity)
+            memberEntity.friends.add(MemberFriendEntity(memberId = memberId, friendId = dto.friendId))
         }
     }
 
